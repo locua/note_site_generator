14184662c7b1344249ad4bf5af56996baaf2cb16
Bob Ud <poes997@gmail.com>
2021-11-14 01:12:30 +0000

    html works on windows not linux

--- homepagegeneratorscript.py
+++ homepagegeneratorscript.py
@@ -1,21 +1,53 @@
 import os
 import re
 
+#print(os.listdir("Working and notes"))
 
-print(os.listdir("Working and notes"))
+p = re.compile('^.+.html$') # html file regex
+htm_fil={} # file list dictionary
+# html header
+main="""<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="styles.css">
+	<title>Notes</title>
+	
+</head>
+<body style="background-color:#f2f2d9">
+<h1> Index of stylus notes </h1>
+"""
 
-p = re.compile('^.+.html')
-
-html_files=[]
-
-for root, dirs, files, in os.walk(".\Working and notes"):
+for root, dirs, files, in os.walk("./Working and notes"):
     for _d in dirs:
-        for files in os.walk(".\Working and notes\/" +_d):
+        htm_fil[_d] = []
+        for files in os.walk("./Working and notes\/" +_d):
             for f in files:
                 if isinstance(f, list) and len(f)>0:
                     for _f in f:
                         if p.match(_f):
-                            #print(_f)
-                            html_files.append(_f)
-                
-print(html_files)
+                            htm_fil[_d].append(_f)
+
+for key in htm_fil:
+    main+="<h2>"+key+"</h2>"
+    main+="<ul>"
+    #print(list_wrap)
+    for file in htm_fil[key]:
+        main+="<li><a href=\""+"./Working and notes/"+key+"/"+file+"\">"+file+"</a></li>"
+    main+="</ul>"
+
+main+="""
+<p style="/*text-align:center*/">🐝</p>
+<!-- END -->
+</body>
+</html>"""
+
+#print("====== test print =======")
+print(main)
+
+# Write to file
+f=open("index.html", "w", encoding="utf-8")
+f.write(main)
+f.close()
+
+
